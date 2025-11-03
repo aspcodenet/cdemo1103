0dcec2791c1cada27296ff0f8a7bc2adb97d35f3
stefan <stefan.holmberg@systementor.se>
2025-11-03 09:28:05 +0100

    v2

--- main.c
+++ main.c
@@ -3,27 +3,72 @@
 //RETURNERA STRÄNGAR!
 
 typedef enum {
-    EN,
-    SE
-} LANG; 
+    ENGLISH,
+    SWEDISH
+} LANG;  
 
-// how NOT to do it:
-char *greet(const char *name, LANG lang) {
 
-    char buffer[50];
-    if(lang == SE)
-        sprintf(buffer, "Hej, %s!", name);
-    else
-        sprintf(buffer, "Hello, %s!", name);
-    return buffer;
+int *add(){
+    int result;
+    result = 12;
+    return &result;
 }
 
-int main() {
-    char *text = greet("Stefan", EN);
-    printf("%s\n", text);
 
-    char *text = greet("Stefan", SE);
-    printf("%s\n", text);
+// RETURNERA DYNAMISKT ALLOKERAD STRÄNG (HEAP)
+char *greetheap(const char *name, LANG lang) {
+    char *texten = malloc(100 * sizeof(char));
+    if(lang == SWEDISH)
+        sprintf(texten, "Hej, %s!", name);
+    else
+        sprintf(texten, "Hello, %s!", name);
+}
+
+ 
+// OM DET ÄR EN STACKALLOKERAD STRÄNG KOMMER DEN FÖRSTÖRAS NÄR FUNKTIONEN AVSLUTAS
+// ALDRIG RETURNERA EN POINTER TILL EN STACKALLOKERAD STRÄNG!
+void greet(const char *name, LANG lang, char *texten) {
+
+    if(lang == SWEDISH)
+        sprintf(texten, "Hej, %s!", name);
+    else
+        sprintf(texten, "Hello, %s!", name);
+}
+
+typedef enum {
+    ERROR_OK,
+    ERROR_INVALID_LANGUAGE,
+    ERROR_NAME_TOO_LONG
+} ERRORS;
+
+char *getErrorString(ERRORS err) {
+    switch(err) {
+        case ERROR_OK:
+            return "No error."; // LIGGER I KONSTANT DATA SEGMENTET  - EJ I STACKWN
+        case ERROR_INVALID_LANGUAGE:
+            return "Invalid language specified.";
+        case ERROR_NAME_TOO_LONG:
+            return "The provided name is too long.";
+        default:
+            return "Unknown error.";
+    }
+}
+
+
+int main() {
+    char *errorMsg = getErrorString(ERROR_NAME_TOO_LONG);
+    printf("Error message: %s\n", errorMsg);
+
+    // char *heapstr = greetheap("Stefan", ENGLISH);
+    // printf("%s\n", heapstr);
+    // free(heapstr);
+
+    char texten[100];
+    greet("Stefan", ENGLISH, texten);
+    printf("%s\n", texten);
+
+    greet("Stefan", SWEDISH,texten);
+    printf("%s\n", texten);
 
     return 0;
 }
